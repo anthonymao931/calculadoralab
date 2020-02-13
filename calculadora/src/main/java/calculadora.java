08a3114f1bc45d1ed93e5d028c690e8182a59b46
Andres Castillo <u1803646@unimilitar.edu.co>
2020-02-12 19:44:36 -0500

    reinicio por error

--- calculadora/src/main/java/calculadora.java
+++ calculadora/src/main/java/calculadora.java
@@ -8,12 +8,12 @@
  *
  * @author Andres
  */
-public class interfaz extends javax.swing.JFrame {
+public class calculadora extends javax.swing.JFrame {
 
     /**
-     * Creates new form interfaz
+     * Creates new form calculadora
      */
-    public interfaz() {
+    public calculadora() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class interfaz extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new interfaz().setVisible(true);
+                new calculadora().setVisible(true);
             }
         });
     }
